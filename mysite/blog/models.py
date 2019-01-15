ada1e4ac90c51510f5fe8cac62bc78657de93c65
euanrussano <euanrussano@gmail.com>
2019-01-15 15:32:36 -0200

    Change to Yves project

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,14 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    commentaire = models.TextField(default='', max_length=300)
+    date_created = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
@@ -17,4 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
